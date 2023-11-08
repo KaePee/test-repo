0c32897e62d99356250603bc0b22c284b1f6f401
Somuah Kwasi Obed <57562618+KaePee@users.noreply.github.com>
2023-11-08 18:59:12 +0000

    Create test.js

---
+++ test.js
@@ -0,0 +1,8 @@
+//Sample code file
+//Js function that searches through an array for the index of an element
+
+function search(arr, val) {
+  for (let i = 0; i < arr.length; i++ ) {
+    //complete function here
+  }
+}
